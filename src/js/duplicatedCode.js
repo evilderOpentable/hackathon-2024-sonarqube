bf5596af9ef508ba307cb79cc8dab689c15d4946
evilderOpentable <evilder@opentable.com>
2024-09-11 10:51:37 -0700

    Duplicated code

--- src/js/duplicatedCode.js
+++ src/js/duplicatedCode.js
@@ -19,4 +19,23 @@ function authenticateUser(username, password) {
     }
 }
 
-module.exports = { authenticateUser };
+function validatePassword(password) {
+    // Check if password exists
+    if (password === "") {
+        return "Password is required";
+    }
+
+    // Check if password length is sufficient
+    if (password.length < 8) {
+        return "Password must be at least 8 characters long";
+    }
+
+    // Simulate password validation
+    if (password === "password123") {
+        return "Valid password";
+    } else {
+        return "Invalid password";
+    }
+}
+
+module.exports = { authenticateUser, validatePassword };
